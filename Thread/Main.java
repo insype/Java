a31487f9d69a5f587447dbd1fc4b1c84d61cb82e
prasiddha <prasiddhadahal33@gmail.com>
2026-01-19 11:32:16 +0545

    thread

---
+++ Thread/Main.java
@@ -0,0 +1,27 @@
+class A extends Thread{
+    public void run(){
+    for(int i=0; i<100; i++){
+        System.out.println("hello");
+    }
+
+    }
+}
+
+class B extends Thread{
+    public void run(){
+    for(int i=0; i<100; i++){
+        System.out.println("hello");
+    }
+
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+       a.start();
+       b.start(); 
+      
+    }
+} 
